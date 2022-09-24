0afd650ec6fbd9ec4f3546784a605f888dde45bd
Oubouabdellah Mohamed <50029907+MohamedOubouabdellah1@users.noreply.github.com>
2022-09-24 15:01:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("/home/ruafbwuf/chatapp/public"));
 
 const botName = "ChatCord Bot";
 // Run when client connects
